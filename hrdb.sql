12c34e3c6ce62cbb9661f164c556528c6debddd9
Nim1010 <nimeshneupane58@gmail.com>
2025-07-09 10:14:29 -0400

    Update hrdb.sql

--- hrdb.sql
+++ hrdb.sql
@@ -12,58 +12,27 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `hrdb`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `admin`
---
 
 INSERT INTO `admin` (`id`, `name`) VALUES
 (1, 'HR Manager');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `employee_detail`
---
 
 CREATE TABLE `employee_detail` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `employee_detail`
---
 
 INSERT INTO `employee_detail` (`id`, `name`) VALUES
 (1, 'Alice Sharma'),
 (2, ''),
 (3, 'Carlo');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `login`
---
 
 CREATE TABLE `login` (
   `id` int(11) NOT NULL,
@@ -71,19 +40,11 @@ CREATE TABLE `login` (
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `login`
---
 
 INSERT INTO `login` (`id`, `email`, `password`) VALUES
 (1, 'admin@hr.com', 'admin123'),
 (2, 'user@hr.com', 'user123');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `salary`
---
 
 CREATE TABLE `salary` (
   `id` int(11) NOT NULL,
@@ -91,83 +52,37 @@ CREATE TABLE `salary` (
   `monthly_salary` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `salary`
---
 
 INSERT INTO `salary` (`id`, `employee_id`, `monthly_salary`) VALUES
 (1, 1, 4000),
 (2, 2, 3500),
 (3, 3, 5000);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `employee_detail`
---
 ALTER TABLE `employee_detail`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `login`
---
 ALTER TABLE `login`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `salary`
---
 ALTER TABLE `salary`
   ADD PRIMARY KEY (`id`),
   ADD KEY `employee_id` (`employee_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- AUTO_INCREMENT for table `employee_detail`
---
 ALTER TABLE `employee_detail`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
---
--- AUTO_INCREMENT for table `login`
---
 ALTER TABLE `login`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `salary`
---
 ALTER TABLE `salary`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `salary`
---
 ALTER TABLE `salary`
   ADD CONSTRAINT `salary_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee_detail` (`id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
